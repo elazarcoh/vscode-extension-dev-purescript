6dd5454f3f96f5ce6f26d1c96b2ee6d4a41f8e98
Elazar <elazarcoh@gmail.com>
2022-03-24 09:11:28 +0200

    add window messages

--- src/VSCode/Window.purs
+++ src/VSCode/Window.purs
@@ -1,10 +1,13 @@
 module VSCode.Window
   ( MessageItem(..)
   , messageItem
+  , showErrorMessage
+  , showErrorMessage'
   , showInformationMessage
   , showInformationMessage'
-  )
-  where
+  , showWarningMessage
+  , showWarningMessage'
+  ) where
 
 import Prelude
 
@@ -17,11 +20,31 @@ import Effect.Aff (Aff)
 newtype MessageItem = MessageItem { title :: String, isCloseAffordance :: Opt Boolean }
 
 foreign import _showInformationMessage :: forall a. String -> Array a -> Effect (Promise a)
+foreign import _showErrorMessage :: forall a. String -> Array a -> Effect (Promise a)
+foreign import _showWarningMessage :: forall a. String -> Array a -> Effect (Promise a)
+
+wrapShowMessage' :: forall a. (String -> Array a -> Effect (Promise a)) -> String -> Array a -> Aff a
+wrapShowMessage' func msg items = toAffE $ func msg items
+
+wrapShowMessage :: forall a. (String -> Array a -> Effect (Promise a)) -> String -> Aff a
+wrapShowMessage func msg = toAffE $ func msg []
 
 showInformationMessage :: String -> Aff Unit
-showInformationMessage msg = toAffE $ _showInformationMessage msg []
+showInformationMessage = wrapShowMessage _showInformationMessage
 
 showInformationMessage' :: String -> Array MessageItem -> Aff MessageItem
-showInformationMessage' msg items = toAffE $ _showInformationMessage msg items
+showInformationMessage' = wrapShowMessage' _showInformationMessage
+
+showErrorMessage :: String -> Aff Unit
+showErrorMessage = wrapShowMessage _showErrorMessage
+
+showErrorMessage' :: String -> Array MessageItem -> Aff MessageItem
+showErrorMessage' = wrapShowMessage' _showErrorMessage
+
+showWarningMessage :: String -> Aff Unit
+showWarningMessage = wrapShowMessage _showWarningMessage
+
+showWarningMessage' :: String -> Array MessageItem -> Aff MessageItem
+showWarningMessage' = wrapShowMessage' _showWarningMessage
 
 messageItem = MessageItem <<< Open.coerce
--- src/VSCode/Window.ts
+++ src/VSCode/Window.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
 
-
 export const _showInformationMessage = msg => items => () => vscode.window.showInformationMessage(msg, undefined, ...items);
+export const _showErrorMessage = msg => items => () => vscode.window.showErrorMessage(msg, undefined, ...items);
+export const _showWarningMessage = msg => items => () => vscode.window.showWarningMessage(msg, undefined, ...items);
